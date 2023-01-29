6e9aef2bf3025fd52759bc79d4dcc4a62c650b01
malczan <97693456+malczan@users.noreply.github.com>
2023-01-29 15:07:25 +0100

    Installed RxSwift with Cocoapods (#5)
    Co-authored-by: Jakub Malczyk <>

---
+++ ChatHub/Views/RegisterView/RegisterConstants.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterConstants.swift
+//  ChatHub
+//
+//  Created by Jakub Malczyk on 29/01/2023.
+//
+
+import Foundation
+
+enum RegisterConstants {
+    
+    static let username = "Username"
+    static let email = "E-mail"
+    static let password = "Password"
+    static let confirmPassword = "Confirm password"
+    
+    static let register = "REGISTER"
+    
+    static let alreadyHaveAccount = "Already have account? Click here to signin"
+    static let alreadyHaveAccountClickable = "Click here to signin"
+    
+}
