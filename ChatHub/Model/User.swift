f8f9650dbdfa3851ae5db203071c3fe693d8945a
Jakub Malczyk <>
2023-02-03 22:27:06 +0100

    Create userService

---
+++ ChatHub/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  ChatHub
+//
+//  Created by Jakub Malczyk on 03/02/2023.
+//
+
+import Foundation
---
+++ ChatHub/Service/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  ChatHub
+//
+//  Created by Jakub Malczyk on 03/02/2023.
+//
+
+import Foundation
